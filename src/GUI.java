50729831e596990147edb49f786e43bf42622748
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-09-26 22:15:05 -0600

    Added skeleton of GUI

---
+++ src/GUI.java
@@ -0,0 +1,23 @@
+import javafx.stage.Stage;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author: Marcus Trujillo
+ * @version: brief class description
+ */
+public class GUI implements Observer {
+
+    public GUI(Stage primaryStage ){
+
+    }
+
+    /**
+     * updates the UI based on the new information of the model
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
