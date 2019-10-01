53f28610b335cd790486eeb5adb59e7879684801
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-10-01 16:58:48 -0600

    can read in file to fill tile pool

--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -10,7 +14,29 @@ public class Model {
     Player player;
     List<Tile> tilePool;
 
-    private void readTilePool(){
+    /**
+     * fills the collection of tiles that can be drawn by reading from a file that contains all inforamation about
+     * the number of each tiles, and the point specifications of each.
+     */
+    private void fillTilePool(){
+        String fileLine = "";
+        try (BufferedReader fileReader = new BufferedReader(new FileReader("res"+File.separator+Constants.TILE_CONFIG_FILE))) {
+            while ((fileLine = fileReader.readLine()) != null) {
+                //parse line into letter, points, and frequency
+                String[] arr = fileLine.split(" ");
+                String letter = arr[0];
+                int pointValue = Integer.parseInt(arr[1]);
+                int frequency = Integer.parseInt(arr[2]);
+                for(int i= 0; i <= frequency; i++) {
+                    tilePool.add( new Tile(letter, pointValue) );
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    public static void main(String[] args){
+        Model model = new Model(); 
     }
 }
